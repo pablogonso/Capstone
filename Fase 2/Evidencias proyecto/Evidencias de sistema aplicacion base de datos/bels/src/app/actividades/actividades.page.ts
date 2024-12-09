2ca7447ea4393c51dd3bb13cbd2452c4b54356b1
josepchile <95584404+josepchile@users.noreply.github.com>
2024-12-09 20:40:41 -0300

    Update actividades.page.ts

--- Fase 2/Evidencias proyecto/Evidencias de sistema aplicacion base de datos/bels/src/app/actividades/actividades.page.ts
+++ Fase 2/Evidencias proyecto/Evidencias de sistema aplicacion base de datos/bels/src/app/actividades/actividades.page.ts
@@ -6,12 +6,12 @@ import { IonicModule } from '@ionic/angular';
 import { ActividadesComponent } from './actividades.component';
 
 @NgModule({
-  declarations: [ActividadesComponent], // Declara los componentes que pertenecen a este módulo
+  declarations: [ActividadesComponent], 
   imports: [
     CommonModule,
     FormsModule,
     IonicModule
   ],
-  exports: [ActividadesComponent] // Exporta los componentes que quieras usar fuera de este módulo
+  exports: [ActividadesComponent] // 
 })
 export class ActividadesModule { }
